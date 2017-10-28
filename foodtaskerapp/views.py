fe03511fffd3a30a0cca4fe6e58de4de6fe36e3e
Makinwa Bukola <bukola.makinwa@andela.com>
2017-10-28 19:40:21 +0100

    feat(auth): authenticate admin user

---
+++ foodtaskerapp/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def home(request):
+    return redirect(restaurant_home)
+
+@login_required(login_url='/restaurant/sign-in/')
+def restaurant_home(request):
+    return render(request, 'restaurant/home.html')
